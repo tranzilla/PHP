38bad5c85337b16e1e199c1ba2b58e9535ddf238
austenwillis <38869666+austenwillis@users.noreply.github.com>
2018-07-31 19:35:21 -0700

    add css

---
+++ foodtruck/foodtruck/footer.php
@@ -0,0 +1,8 @@
+<?php
+$myDate = date ('o')
+?>
+<footer>
+<p><small>&copy; <?=$myDate?> by Tran, Austen, Mike<p>
+</footer>
+</body>
+</html>
---
+++ foodtruck/foodtruck/header.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Newmanix Burgers</title>
+<meta name="viewport" content="width=device-width">
+<meta name="robots" content="no index,no follow">
+<meta charset="utf-8">
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header> 
+    <h1>Welcome to Newmanix Burgers</h1>   
+</header>
+<body>
